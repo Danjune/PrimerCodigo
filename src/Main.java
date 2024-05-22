6ba198aa1b66b5abe8ca356e64e236b79489a611
Danjune <daniel.ayala621@gmail.com>
2024-05-21 20:13:35 -0600

    Code 1: Intercambiar Variables

--- src/Main.java
+++ src/Main.java
@@ -3,17 +3,20 @@
 public class Main {
     public static void main(String[] args) {
 
+        //Variables a intercambiar
         int varA = 35;
         int varB = 20;
 
+        //Mostrar valores primero
         System.out.println("varA: " + varA );
         System.out.println("varB: " + varB );
 
+        //Usar una variable auxiliar y cambiar los valores
         int cola = varA;
         varA = varB;
         varB = cola;
 
-
+        //Mostrar los valores nuevos.
         System.out.println("Now:");
         System.out.println("varA: " + varA);
         System.out.println("varB: " + varB);
